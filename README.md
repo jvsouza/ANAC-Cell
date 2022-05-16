088e28f07715cd9c59fe1323754acab429c1846a
Jonas Vieira de Souza <jonas.vieira.de.souza@gmail.com>
2022-05-16 00:40:49 -0300

    Initial commit
    up !

---
+++ README.md
@@ -0,0 +1,62 @@
+# ANAC-Cell
+![Progress](https://progress-bar.dev/27/?title=Completed%23&width=160&color=54aeff)
+
+## Collection of questions for the preparatory study of the theoretical exam of ANAC in aeronautical maintenance mechanics, cell group.
+> [As specified by ANAC](https://www.gov.br/anac/pt-br/assuntos/regulados/profissionais-da-aviacao-civil/processo-de-licencas-e-habilitacoes/arquivos/Port41452021ExamesteoricoseAnexos.pdf), the CHT (certificate of technical skill) is issued upon passing a theoretical knowledge exam in the respective category of activity.
+
+## For aeronautical maintenance mechanic, avionics group, the exam is composed of the basic and avionics modules. As table bellow:
+| Type | Index | Progress | Contet |
+| :--: | :---: | :------: | :----- |
+| Cel | 01 | ![](https://img.shields.io/badge/-ToDo-inactive) | Technical English |
+| Cel | 02 | ![](https://img.shields.io/badge/-ToDo-inactive) | Aircraft Structures And Flight Control Systems |
+| Cel | 03 | ![](https://img.shields.io/badge/-ToDo-inactive) | Fabricing |
+| Cel | 04 | ![](https://img.shields.io/badge/-ToDo-inactive) | Coating, Painting And Finishing Of Aircraft |
+| Cel | 05 | ![](https://img.shields.io/badge/-ToDo-inactive) | Repairs To Metallic Structures |
+| Cel | 06 | ![](https://img.shields.io/badge/-Done-success) | Welding In Aircraft |
+| Cel | 07 | ![](https://img.shields.io/badge/-ToDo-inactive) | Advanced Compound Materials |
+| Cel | 08 | ![](https://img.shields.io/badge/-ToDo-inactive) | Aircraft Electrical Systems |
+| Cel | 09 | ![](https://img.shields.io/badge/-ToDo-inactive) | Aircraft Instruments |
+| Cel | 10 | ![](https://img.shields.io/badge/-ToDo-inactive) | Communication And Navigation Systems |
+| Cel | 11 | ![](https://img.shields.io/badge/-Done-success) | Hydraulic And Landing Gear System |
+| Cel | 12 | ![](https://img.shields.io/badge/-ToDo-inactive) | Protection Systems Against The Effects Of Ice And Rain And Against Fire |
+| Cel | 13 | ![](https://img.shields.io/badge/-ToDo-inactive) | Pneumatic And Cabin Environment Control Systems |
+
+
+## For that, I'm storing studied questions in this collection. As webpage struct bellow:
+```text
+page
+├── css
+│   ├── bootstrap.min.css
+│   └── custom.css
+├── js
+│   ├── app.py
+│   ├── bootstrap.min.py
+│   └── jquery.min.py
+├── json
+│   ├── cel06.json
+│   └── cel11.json
+├── README.md
+├── favicon.png
+└── index.html
+
+```
+
+> Link of Github Page created: https://jvsouza.github.io/ANAC-Cell.github.io/
+
+## Shields
+[![](https://img.shields.io/github/languages/top/jvsouza/ANAC-Cell.github.io)]()
+[![](https://img.shields.io/github/languages/count/jvsouza/ANAC-Cell.github.io)]()
+[![](https://img.shields.io/github/license/jvsouza/ANAC-Cell.github.io)]()
+
+[![](https://img.shields.io/github/languages/code-size/jvsouza/ANAC-Cell.github.io)]()
+[![](https://img.shields.io/github/repo-size/jvsouza/ANAC-Cell.github.io)]()
+[![](https://img.shields.io/github/last-commit/jvsouza/ANAC-Cell.github.io)]()
+
+## Badges
+![HTML](https://img.shields.io/badge/HTML-239120?style=for-the-badge&logo=html5&logoColor=white)
+![CSS3](https://img.shields.io/badge/css3-%231572B6.svg?style=for-the-badge&logo=css3&logoColor=white)
+![JavaScript](https://img.shields.io/badge/javascript-%23323330.svg?style=for-the-badge&logo=javascript&logoColor=%23F7DF1E)
+
+![HTML5](https://img.shields.io/badge/html5-%23E34F26.svg?style=for-the-badge&logo=html5&logoColor=white)
+![Bootstrap](https://img.shields.io/badge/bootstrap-%23563D7C.svg?style=for-the-badge&logo=bootstrap&logoColor=white)
+![jQuery](https://img.shields.io/badge/jquery-%230769AD.svg?style=for-the-badge&logo=jquery&logoColor=white)
---
+++ js/app.js
@@ -0,0 +1,112 @@
+function num2letter( number ){
+    if (number == 0) {
+        return "A";
+    } else if (number == 1) {
+        return "B";
+    } else if (number == 2) {
+        return "C";
+    } else {
+        return "D";
+    }
+}
+// ...
+function createList( arrayJson ) {
+    var questions = arrayJson.questions;
+
+    var list = '<ul class="ulTop list-group">';
+    var letter = '';
+    for (var n=0; n < questions.length ; n++) {
+        list += '<li class="liTop list-group-item" data-li="'+n+'">';   
+            list += '<ul class="ulMid list-group">';
+                list += '<li class="liMed title list-group-item">';
+                    list += '<div class="row">';
+                        list += '<div class="col-1 text-center align-items-center justify-content-center d-inline-flex">';
+                            list += parseInt(n+1, 10);
+                        list += '</div>';
+                        list += '<div class="col-9">';
+                            list += questions[n].title;
+                        list += '</div>';
+                        list += '<div class="col-2">';
+                            list += '<button type="button" class="btn btn-outline-primary w-100 h-100" data-question="'+ n +'">Verificar</button>';
+                        list += '</div>';
+                    list += '</div>';
+                list += '<li class="liMed options">';
+                    list += '<div class="list-group" data-select="'+n+'">';
+                        for (var i = 0; i < questions[n].options.length; i++) {
+                            list += '<button type="button" class="list-group-item list-group-item-action" value="'+ i +'">' + num2letter(i) + ': '+ questions[n].options[i] + '</button>';
+                        }
+                    list += '</div>';
+                list += '</li>';
+            list += '</ul>';
+        list += '</li>';
+    }
+    list += '</ul>';
+    $("#questions").append(list);
+}
+// ...
+function getJson( nameFileJson ) {
+    var result;
+    $.ajax({
+        dataType: "json",
+        url: nameFileJson,  
+        async: false,  
+        success: function(data) {
+            result = data; 
+      }
+    });
+    return result;
+}
+// ...
+function openFile(file) {
+    var extension = file.substr( (file.lastIndexOf('.') +1) );
+    switch(extension) {
+        case 'json':
+            return true;
+        default:
+            return false;
+    }
+};
+// ...
+function getListJson( baseFolder, filesJson ){
+    console.log(">getListJson");
+    var fileNames = new Array();
+    for (var n=0; n < filesJson.length ; n++) {
+        fileNames.push(baseFolder+filesJson[n]+".json");
+    }
+    return fileNames;
+}
+// ...
+function createSelect( arrayJson ){
+    console.log(">createSelect");
+    var options = '<option value="title" selected>Select content</option>';
+    for (var n=0; n < arrayJson.length ; n++) {
+        courseUnit = getJson(arrayJson[n]).courseunit;
+        options += '<option value="' + arrayJson[n] + '">' + courseUnit + '</option>';
+    }
+     $("#select").append(options);
+}
+// ...
+$(document).ready(function(){
+
+    var arrayJson;
+    // ...
+    $(document).on('click','.btn', function() {
+        let questionNumber = $(this).data('question');
+        let correctOption = arrayJson.questions[questionNumber].result;
+        $("li[data-li="+questionNumber+"] li.options div button[value="+correctOption+"]").addClass( "optionCorrect" );
+    });
+    // ...
+    $(document).on("change","#select",function(){
+        let nameJson = $(this).val();
+        $("#questions").empty();
+        if (nameJson != 'title'){
+            arrayJson = getJson(nameJson);
+            createList(arrayJson);            
+        }
+    });
+
+    base = 'https://raw.githubusercontent.com/jvsouza/ANAC-Cell.github.io/master/json/';
+    filesJson = ['bas06','bas11'];
+    createSelect(getListJson(base, filesJson));
+
+});
